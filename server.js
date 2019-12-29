1b436a2992ceef482bbbb31a5c7a5d37b4e213a6
Alice Russell <alice.louise.russell@gmail.com>
2019-12-29 11:33:18 -0500

    install and require npm dependencies

---
+++ server.js
@@ -0,0 +1,18 @@
+// dependencies 
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+var app = express();
+
+// set port
+var PORT = process.env.PORT || 8080;
+
+// set express to handle data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// set handlebars as default templating engine 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
