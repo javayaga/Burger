7d07fa28618e3539ca79fe50057d61f1174834bf
Alice Russell <alice.louise.russell@gmail.com>
2020-01-03 18:36:40 -0500

    intial controllers created

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+// import
+// express
+const express = require("express");
+
+// router
+const router = express.Router();
+
+// burger
+const burger = require("../models/burger");
+
+
+// routes 
+// get all 
+
+// create 
+
+// update
